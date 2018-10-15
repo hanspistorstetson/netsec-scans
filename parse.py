daecba9bb0770666f08262eb264ceed90e6d719e
Hans Pistor <hpistor@stetson.edu>
2018-10-15 14:08:25 -0400

    added parsed scans

--- parse.py
+++ parse.py
@@ -1,6 +1,10 @@
 import sys
+import re
 
-# FIND ONES WITH PORT 80 OPEN
-num_lines = sum(1 for line in open('scans/tcpack.scan'))
-
-print(num_lines)
+with open('scans/tcpsyn.scan') as f:
+    current_ip = None
+    for line in f.readlines():
+        if 'Nmap scan report for ' in line:
+            current_ip = re.findall(r'([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', line)[0]
+        if '23/tcp' in line and 'open' in line:
+            print(current_ip)

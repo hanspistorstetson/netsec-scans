1fd0e33a46e7f2862156a7c962784ab82ed46f70
Hans Pistor <hpistor@stetson.edu>
2018-10-22 16:12:26 -0400

    custom ports

--- parse.py
+++ parse.py
@@ -1,10 +1,20 @@
 import sys
 import re
 
+ips = set()
+
 with open('scans/tcpsyn.scan') as f:
     current_ip = None
     for line in f.readlines():
         if 'Nmap scan report for ' in line:
             current_ip = re.findall(r'([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', line)[0]
-        if '23/tcp' in line and 'open' in line:
-            print(current_ip)
+        else:
+            test = re.findall(r'[\d]+', line)
+            if len(test) == 1:
+                port = int(test[0])
+                if (port > 1023):
+                    ips.add(current_ip)
+
+
+for ip in ips:
+    print(ip)
